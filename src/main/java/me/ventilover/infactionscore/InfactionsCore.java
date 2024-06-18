c001a1d200a4368c6b8e14e652178c178dab8112
BuildTools <unconfigured@null.spigotmc.org>
2024-06-18 19:39:16 +0200

    First commit just the base for the plugin

---
+++ src/main/java/me/ventilover/infactionscore/InfactionsCore.java
@@ -0,0 +1,17 @@
+package me.ventilover.infactionscore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class InfactionsCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
