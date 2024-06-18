d08ddaeb9e02895a3e8e957839fb24b8981a2be2
BuildTools <unconfigured@null.spigotmc.org>
2024-06-18 19:43:35 +0200

    made some basic skeleton

--- src/main/java/me/ventilover/infactionscore/InfactionsCore.java
+++ src/main/java/me/ventilover/infactionscore/InfactionsCore.java
@@ -1,17 +1,29 @@
 package me.ventilover.infactionscore;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InfactionsCore extends JavaPlugin {
 
+    //main class for the plugin
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+
+
+        getServer().getPluginManager().registerEvents(null,this); //register event listener here
+
+        //create the commandMap for commands
+        CommandMap commandMap = Bukkit.getCommandMap();
+
+        getLogger().info("InfactionsCore has started!");//logger message
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("InfactionsCore has stopped!");
     }
 }
