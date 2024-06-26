ae561736d43049cfef76bb9ae0082942af71a427
BuildTools <unconfigured@null.spigotmc.org>
2024-06-26 20:33:29 +0200

    Command to handle all the other commands and wrote commands

--- src/main/java/me/ventilover/infactionscore/FactionCommand.java
+++ src/main/java/me/ventilover/infactionscore/FactionCommand.java
@@ -1,47 +1,53 @@
 package me.ventilover.infactionscore;
 
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
+public class FactionCommand extends Command {
+    //this is the basic command for handling the other ones
+    //e.g /faction list
 
-
-public class FactionCommand extends Command { //command to see all other faction related commands
-
-    //constructor of the command
-    protected FactionCommand() {
+    protected FactionCommand(){ //constructor
         super("faction");
-        this.description = "Command to see available faction commands";
-        this.usageMessage = "/faction";
-        this.setAliases(List.of("factions"));
+        this.description = "Basic faction command"; //command description
+        this.usageMessage = "/faction [command]";
     }
 
-    @Override //execute method called when the command is executed
+    @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
-        if (!checkIfPermission(sender)){
-            return false; //check the permission
+        //this method is called when the command gets executed
+
+        if (CommandManager.getInstance().checkIfPlayerNoPermission(sender, "infactions.factioncommand")) {//first check if the sender has the permission
+            sender.sendMessage("No permission");//if not send an error message
+            return false;
+        }
+
+        if (args.length == 1){ //check if we have one argument (for the specific command name)
+            if (commandExist(args[0])){ //if the command exist dispatch it via the hashmap
+                Bukkit.dispatchCommand(sender, CommandManager.getCommandHashMap().get(args[0].toLowerCase()));
+            }
+            else {
+                Bukkit.dispatchCommand(sender, "fhelp"); //else display basic help command
+
+            }
         }
         else {
-            sender.sendMessage(buildHelpMessage());//send the player the textmessage
-            return true;
+            Bukkit.dispatchCommand(sender, "fhelp"); //if there's no or more than one argument basic help command
+        }
+        return true;
+    }
+
+    public boolean commandExist(String string){ //method to check if a command exists in the hashmap
+        boolean result = false;
+        for (String name : CommandManager.getCommandHashMap().keySet()){ //check the key set
+            if (string.equalsIgnoreCase(name)){//ignore case sensitivity
+                result = true;// loop through the map, and if we find it, break the loop
+                break;
+            }
         }
 
-    }
-
-    public Component buildHelpMessage(){
-        final TextColor customRedColor = TextColor.fromHexString("#e32064");
-        final TextColor lighterRedColor = TextColor.fromHexString("#ff6666");
-        return Component.text()
-                .append(Component.text("Here are some useful Commands: ", customRedColor))
-                .append(Component.newline())
-                .append(Component.text("/faction list"))
-                .build();
-    }
-
-    public boolean checkIfPermission(CommandSender sender){
-        return sender.hasPermission("infactionscore.faction");
+        return result; //return the boolean
     }
 }
