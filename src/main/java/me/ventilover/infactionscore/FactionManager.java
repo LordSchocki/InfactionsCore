190eb76ff3b09cc668cd9c0ad69eee17bfae41eb
BuildTools <unconfigured@null.spigotmc.org>
2024-06-27 18:39:22 +0200

    New Method to find the faction of a player and method to see if a player is even in an faction

--- src/main/java/me/ventilover/infactionscore/FactionManager.java
+++ src/main/java/me/ventilover/infactionscore/FactionManager.java
@@ -1,5 +1,8 @@
 package me.ventilover.infactionscore;
+import org.bukkit.entity.Player;
+
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 
 public class FactionManager {//singleton class
@@ -27,6 +30,30 @@ public class FactionManager {//singleton class
 
     public void removeFactionFromArrayList(Faction faction){
         factionArrayList.remove(faction);
+    }
+
+    public Faction getFactionFromPlayer(Player player) throws PlayerNotInFactionException ,NoSuchElementException{
+        if (playerMemberOfAnFaction(player)) {
+            for (Faction faction : factionArrayList){
+                if (faction.playerInFaction(player)){
+                    return faction;
+                }
+            }
+            throw new NoSuchElementException("Player not found in a faction");
+        }
+        else {
+            throw new PlayerNotInFactionException("Player is not in a faction");
+        }
+
+    }
+
+    public boolean playerMemberOfAnFaction(Player player){ //check if a player is a member of a faction
+        for (Faction faction : factionArrayList){
+            if (faction.playerInFaction(player)){ //loop through the array and check
+                return true;// if the player is found
+            }
+        }
+        return false;//if not found in any faction, return false
     }
 
 
