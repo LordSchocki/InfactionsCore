5c175fb9fc5636630a4aa0b857b2324e53111e60
BuildTools <unconfigured@null.spigotmc.org>
2024-06-30 18:55:42 +0200

    added new methods to safe factionarraylist into an ser file comments will follow

--- src/main/java/me/ventilover/infactionscore/DataSafeManager.java
+++ src/main/java/me/ventilover/infactionscore/DataSafeManager.java
@@ -1,5 +1,8 @@
 package me.ventilover.infactionscore;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class DataSafeManager { //singleton class
     //this class is here to manage all the data that needs to be saved via methods for each safe data
 
@@ -9,7 +12,7 @@ public class DataSafeManager { //singleton class
         //block constructor
     }
 
-    public DataSafeManager getInstance() {
+    public static DataSafeManager getInstance() {
         if (instance == null){
             instance = new DataSafeManager(); //getter, for instance
         }
@@ -20,5 +23,51 @@ public class DataSafeManager { //singleton class
     public void safeAllData(){
         //logic to safe later data
         //this ist the method for all the sub methods like safe factions names etc...
+        saveFactionsToFile();
+    }
+    public void loadAllData(){
+        loadFactionsFromFile();
+    }
+
+    public void saveFactionsToFile() {
+        if (FactionManager.getInstance().factionArrayList.isEmpty()){
+            InfactionsCore.instance.getLogger().info("NO FACTIONS TO SAFE");
+            return;
+        }
+
+        try {
+            FileOutputStream fileOut = new FileOutputStream("factions.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(FactionManager.getInstance().factionArrayList);
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            InfactionsCore.instance.getLogger().info(e.getMessage());
+        }
+    }
+    @SuppressWarnings("unchecked")
+    public void loadFactionsFromFile() {
+
+        File file = new File("factions.ser");
+        if (!file.exists()){
+            InfactionsCore.instance.getLogger().info("FILE DOES NOT EXIST");
+            return;
+        }
+
+        try {
+            FileInputStream fileIn = new FileInputStream("factions.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            Object obj = in.readObject();
+            if (obj instanceof ArrayList) {
+                //noinspection unchecked
+
+                FactionManager.getInstance().factionArrayList = (ArrayList<Faction>) obj;
+                InfactionsCore.instance.getLogger().info("CLASS NOT INSTANCE OF ARRAYLIST");
+            }
+            in.close();
+            fileIn.close();
+        } catch (IOException | ClassNotFoundException e) {
+            InfactionsCore.instance.getLogger().info(e.getMessage());
+        }
     }
 }
